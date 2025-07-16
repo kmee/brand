6d99c938e55039b0ad866d27912bb6f05f7b4903
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-16 11:57:31 -0300

    [MIG] product_brand_mrp: Migration to 17.0

---
+++ product_brand_mrp/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Product Brand MRP',
+    'summary': 'Permite trabalhar com product_brand no MRP.',
+    'version': '17.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/brand',
+    'depends': ['product_brand', 'mrp'],
+    'data': ['views/mrp_production_views.xml'],
+    'installable': True,
+    'development_status': 'Beta'
+}
