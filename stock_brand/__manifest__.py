a659df4fb41d53d466f60d95e6a82df6606e0255
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-16 11:57:31 -0300

    [MIG] stock_brand: Migration to 17.0

---
+++ stock_brand/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Stock Brand',
+    'summary': 'Gerencia marcas em documentos de picking do estoque.',
+    'version': '17.0.1.0.0',
+    'category': 'Warehouse',
+    'website': 'https://github.com/OCA/brand',
+    'author': 'Sunflower IT, Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'depends': ['sale_brand', 'sale_stock'],
+    'data': ['views/stock_picking_views.xml'],
+    'installable': True,
+    'development_status': 'Beta'
+}
