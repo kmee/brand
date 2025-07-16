1e19bcb41a91fe7e66d2e588167dbcdacc38e11c
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-16 11:57:31 -0300

    [MIG] product_brand_stock_account: Migration to 17.0

---
+++ product_brand_stock_account/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Product Brand Stock Account',
+    'summary': 'Permite trabalhar com product_brand em Stock Account.',
+    'version': '17.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/brand',
+    'depends': ['product_brand', 'stock_account'],
+    'data': ['views/stock_valuation_layer_views.xml'],
+    'installable': True,
+    'development_status': 'Beta'
+}
